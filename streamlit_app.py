a49722e2b3bcd67905c3f3ea9b14e76bfe43e167
MONDIIT120 <145890130+MONDIIT120@users.noreply.github.com>
2023-10-05 19:34:34 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+st.set_page_config(page_title="games", page_icon=":tada:",layout="wide")
+with st.container():
+ st.subheader("HELLO, GOODMORNING :satisfied:,OH INCASE I DONT SE YA GOODAFTERNOON :hushed:,GOODEVENING:kissing: and GOODNIGHT :wave:")
+ st.title("GAMES TO BUY : ")
+ st.write("WE LOVE VIDEO GAMES AND ARE EAGERLY WAITING TO SHOW YOU OUR MOST BELOVED VIDEO GAMES WITH THE LINK TO BUY THEM")
+ st.write("Companies : ROCCKSTAR GAMES ,SONY, ACTIVISION")
+ st.write("Links of some of the most used websites to buy video games 👇")
+ st.write("[Learn more > SONY](https://www.bing.com/ck/a?!&&p=6003754d24ebb935JmltdHM9MTY5NTQyNzIwMCZpZ3VpZD0zMzkwYWM1Yy1jYjhhLTYzODAtMWMzYy1iZWRhY2ExMTYyOTMmaW5zaWQ9NTIzNA&ptn=3&hsh=3&fclid=3390ac5c-cb8a-6380-1c3c-bedaca116293&psq=activision&u=a1aHR0cHM6Ly93d3cuYWN0aXZpc2lvbi5jb20v&ntb=1)")
+ st.write("[Learn more > ROCKSTAR GAMES](https://www.bing.com/ck/a?!&&p=fc1790bbf283402fJmltdHM9MTY5NTQyNzIwMCZpZ3VpZD0zMzkwYWM1Yy1jYjhhLTYzODAtMWMzYy1iZWRhY2ExMTYyOTMmaW5zaWQ9NTIxNQ&ptn=3&hsh=3&fclid=3390ac5c-cb8a-6380-1c3c-bedaca116293&psq=rockstar+games&u=a1aHR0cHM6Ly93d3cucm9ja3N0YXJnYW1lcy5jb20v&ntb=1)")
+ st.write("[Learn more > STEAM](https://www.bing.com/ck/a?!&&p=4934b7844a1c5ccbJmltdHM9MTY5NTQyNzIwMCZpZ3VpZD0zMzkwYWM1Yy1jYjhhLTYzODAtMWMzYy1iZWRhY2ExMTYyOTMmaW5zaWQ9NTIxOQ&ptn=3&hsh=3&fclid=3390ac5c-cb8a-6380-1c3c-bedaca116293&psq=STEAM&u=a1aHR0cHM6Ly9zdG9yZS5zdGVhbXBvd2VyZWQuY29tLw&ntb=1)")
+ st.write("[Learn More > EPIC GAMES](https://www.bing.com/ck/a?!&&p=2bd977935a94cdb3JmltdHM9MTY5NTQyNzIwMCZpZ3VpZD0zMzkwYWM1Yy1jYjhhLTYzODAtMWMzYy1iZWRhY2ExMTYyOTMmaW5zaWQ9NTIzMw&ptn=3&hsh=3&fclid=3390ac5c-cb8a-6380-1c3c-bedaca116293&psq=epic+games+store&u=a1aHR0cHM6Ly9zdG9yZS5lcGljZ2FtZXMuY29tL2VuLVVTLw&ntb=1)")
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("OUR AIM")
+        st.write("##")
+        st.write(
+            """
+            BY CREATING THIS WEBSITE WE ARE TRYING TO HELP PEOPLE TO FIND AND BUY THEIR FAVORITE VIDEO GAMES LIKE :
+            - GTA SERIES
+            - RDR SERIES
+            - CALL OF DUTY
+            - LAST OF US
+            - FORRZA HORIZON SERIES
+            - TOMB RAIDER SERIES 
+            
+            
+            
+            
+            
+            
+            IF THIS SOUNDS INTERSTING TO YOU , THANK YOU. ✌
+            """
+        )
